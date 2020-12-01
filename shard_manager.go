8c943f7cf5864113476e08ce5f640eac568823aa
sirMackk <matt@mattscodecave.com>
2020-11-30 18:06:30 -0800

    Expose ShardManager struct members

--- shard_manager.go
+++ shard_manager.go
@@ -9,27 +9,27 @@ import (
 )
 
 type ShardManager struct {
-	dataSources []io.ReadWriteSeeker
-	metadata    *Metadata
+	DataSources []io.ReadWriteSeeker
+	Metadata    *Metadata
 }
 
 func NewShardManager(src []io.ReadWriteSeeker, meta *Metadata) *ShardManager {
 	return &ShardManager{
-		dataSources: src,
-		metadata:    meta,
+		DataSources: src,
+		Metadata:    meta,
 	}
 }
 
 func (p *ShardManager) findCorruptShards() ([]int, error) {
 	brokenShards := make([]int, 0)
-	for i := 0; i < len(p.metadata.Hashes); i++ {
+	for i := 0; i < len(p.Metadata.Hashes); i++ {
 		hasher := sha256.New()
-		defer p.dataSources[i].Seek(0, 0)
-		_, err := io.Copy(hasher, p.dataSources[i])
+		defer p.DataSources[i].Seek(0, 0)
+		_, err := io.Copy(hasher, p.DataSources[i])
 		if err != nil {
 			return nil, fmt.Errorf("Error hashing shard %d: %s", i, err)
 		}
-		if newHash := fmt.Sprintf("%x", hasher.Sum(nil)); newHash != p.metadata.Hashes[i] {
+		if newHash := fmt.Sprintf("%x", hasher.Sum(nil)); newHash != p.Metadata.Hashes[i] {
 			brokenShards = append(brokenShards, i)
 		}
 	}
@@ -37,7 +37,7 @@ func (p *ShardManager) findCorruptShards() ([]int, error) {
 }
 
 func (p *ShardManager) Read(dataDst io.Writer) error {
-	for _, dataSource := range p.dataSources[:p.metadata.DataShards] {
+	for _, dataSource := range p.DataSources[:p.Metadata.DataShards] {
 		_, err := io.Copy(dataDst, dataSource)
 		if err != nil {
 			return fmt.Errorf("Error while reading: %s", err)
@@ -66,25 +66,25 @@ func (p *ShardManager) Repair() error {
 		return nil
 	}
 
-	if bsCount := len(brokenShardIndexes); bsCount > p.metadata.ParityShards {
-		return fmt.Errorf("Cannot repair data: %d shards corrupt, only have %d parity shards", bsCount, p.metadata.ParityShards)
+	if bsCount := len(brokenShardIndexes); bsCount > p.Metadata.ParityShards {
+		return fmt.Errorf("Cannot repair data: %d shards corrupt, only have %d parity shards", bsCount, p.Metadata.ParityShards)
 	}
 
-	shardCount := p.metadata.DataShards + p.metadata.ParityShards
+	shardCount := p.Metadata.DataShards + p.Metadata.ParityShards
 	shardReaders := make([]io.Reader, shardCount)
 	shardWriters := make([]io.Writer, shardCount)
 
-	for i := range p.dataSources {
-		shardReaders[i] = p.dataSources[i].(io.Reader)
+	for i := range p.DataSources {
+		shardReaders[i] = p.DataSources[i].(io.Reader)
 	}
 
 	// mark shards as broken, mark which shards to write
 	for _, shardIndex := range brokenShardIndexes {
 		shardReaders[shardIndex] = nil
-		shardWriters[shardIndex] = p.dataSources[shardIndex].(io.Writer)
+		shardWriters[shardIndex] = p.DataSources[shardIndex].(io.Writer)
 	}
 
-	RSEncoder, err := reedsolomon.NewStream(p.metadata.DataShards, p.metadata.ParityShards)
+	RSEncoder, err := reedsolomon.NewStream(p.Metadata.DataShards, p.Metadata.ParityShards)
 	if err != nil {
 		return fmt.Errorf("Error creating reedsolomon encoder: %s", err)
 	}
